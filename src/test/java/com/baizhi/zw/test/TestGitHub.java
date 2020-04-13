bc251d74e3c1abd1c4229aec5068d45599100a32
zhaowei <18740690154@163.com>
2020-04-14 00:49:52 +0800

    第二次修改GitHUb测试类

--- src/test/java/com/baizhi/zw/test/TestGitHub.java
+++ src/test/java/com/baizhi/zw/test/TestGitHub.java
@@ -16,7 +16,7 @@ public class TestGitHub {
     public void testGitHub(){
         int a = 10;
         int b = 20;
-        system.out.println("a+b="+a+b);
+        System.out.println("a+b="+a+b);
     }
 
 
